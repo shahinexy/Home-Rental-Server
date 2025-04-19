7d90e892503656d3888d320db51f5dfb8a5f7525
Shahin Alam <shahinexy@gmail.com>
2025-04-19 12:34:51 +0600

    dist removed

--- dist/app/modules/Property/property.costant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.propertySearchAbleFields = exports.propertyFilterableFields = void 0;
-exports.propertyFilterableFields = [
-    "searchTerm",
-    "location",
-    "contractExpiresAt"
-];
-exports.propertySearchAbleFields = ["name"];
